4467e85000cb00ca81bd39158af4c56b971896db
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-30 19:14:16 +0100

    create and test root for finding a folder by its id

--- FileStructure/src/main/java/com/example/codeclan/FileStructure/controllers/FolderController.java
+++ FileStructure/src/main/java/com/example/codeclan/FileStructure/controllers/FolderController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,11 @@ public class FolderController {
         return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
     }
 
-    
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity getFolder(@PathVariable Long id){
+        return new ResponseEntity<>(folderRepository.findById(id), HttpStatus.OK);
+
+    }
 
 
 }
