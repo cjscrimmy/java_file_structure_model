4790e8305345643d729530476591f7d18eff6494
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-30 15:37:11 +0100

    initial

---
+++ FileStructure/src/main/java/com/example/codeclan/FileStructure/FileStructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.FileStructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStructureApplication.class, args);
+	}
+
+}
---
+++ FileStructure/src/test/java/com/example/codeclan/FileStructure/FileStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.FileStructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
