9e6f5e8f3f7cd5ea69e700044bc2061ff28dc957
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-30 19:11:25 +0100

    add and test root for getting all the folders

--- FileStructure/src/main/java/com/example/codeclan/FileStructure/controllers/FolderController.java
+++ FileStructure/src/main/java/com/example/codeclan/FileStructure/controllers/FolderController.java
@@ -1,4 +1,27 @@
 package com.example.codeclan.FileStructure.controllers;
 
+import com.example.codeclan.FileStructure.models.Folder;
+import com.example.codeclan.FileStructure.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class FolderController {
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders(){
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    
+
+
 }
