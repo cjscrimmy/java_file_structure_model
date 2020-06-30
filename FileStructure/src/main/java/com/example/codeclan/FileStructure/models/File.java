788c0bdd7d1239616b3cffd407499c273e9a37e4
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-30 16:04:27 +0100

    start coding folder and file classes

--- FileStructure/src/main/java/com/example/codeclan/FileStructure/models/File.java
+++ FileStructure/src/main/java/com/example/codeclan/FileStructure/models/File.java
@@ -1,4 +1,23 @@
 package com.example.codeclan.FileStructure.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="files")
 public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="extension")
+    private String name;
+
+
+
 }
--- FileStructure/src/main/java/com/example/codeclan/FileStructure/models/Folder.java
+++ FileStructure/src/main/java/com/example/codeclan/FileStructure/models/Folder.java
@@ -1,4 +1,63 @@
 package com.example.codeclan.FileStructure.models;
 
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
 public class Folder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    
+    private List<File> files;
+
+    private User user;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.files = new ArrayList<>();
+        this.user = user;
+    }
+
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
